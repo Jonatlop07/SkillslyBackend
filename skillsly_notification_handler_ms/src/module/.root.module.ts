d61954fa33da6c813007ba673da8ea85bfd207b0
Jonathan Lopez Castellanos <jolopezca@unal.edu.co>
2022-05-01 11:15:13 -0500

    Setting up Kubernetes deployment configuration files

--- skillsly_notification_handler_ms/src/module/.root.module.ts
+++ skillsly_notification_handler_ms/src/module/.root.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { setEnvironment } from '../environments';
 import { NotificationHandlerModule } from '../notification_handler/notification_handler.module'
-import { InfrastructureModule } from './infrastructure.module'
 
 @Module({
   imports: [
@@ -10,7 +9,6 @@ import { InfrastructureModule } from './infrastructure.module'
       isGlobal: true,
       envFilePath: `env/${setEnvironment()}`,
     }),
-    InfrastructureModule,
     NotificationHandlerModule
   ],
 })
--- skillsly_notification_handler_ms/src/notification_handler/notification_handler.module.ts
+++ skillsly_notification_handler_ms/src/notification_handler/notification_handler.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common'
+import { CacheModule, Module } from '@nestjs/common'
 import { NotificationHandlerController } from './notification_handler.controller'
 import { NotificationHandlerService } from './notification_handler.service'
 import { NotificationHandlerGateway } from './notification_handler.gateway'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import * as redis_store from 'cache-manager-redis-store';
 
 @Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config_service: ConfigService) => ({
+        store: redis_store,
+        host: config_service.get('REDIS_HOST'),
+        port: config_service.get('REDIS_PORT'),
+        ttl: config_service.get('TTL_CACHE'),
+        isGlobal: true
+      })
+    })
+  ],
   controllers: [NotificationHandlerController],
   providers: [NotificationHandlerService, NotificationHandlerGateway]
 })
