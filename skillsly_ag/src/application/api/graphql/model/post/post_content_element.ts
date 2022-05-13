5857fa2f9ef66738803c9475ff81536e20757e5a
Victor Barragan <vbarraganp@unal.edu.co>
2022-05-13 01:46:06 -0500

    media_type added to post model

--- skillsly_ag/src/application/api/graphql/model/post/post_content_element.ts
+++ skillsly_ag/src/application/api/graphql/model/post/post_content_element.ts
@@ -1,14 +1,13 @@
-import {Field, ID, ObjectType} from '@nestjs/graphql';
-import {Id} from '@application/common/type/common_types';
+import {Field, ObjectType} from '@nestjs/graphql';
 
 @ObjectType({ description: 'Defines the content of a post' })
 export class ContentElement {
-  @Field(() => ID)
-  public content_element_id: Id;
-
   @Field()
   public description: string;
 
   @Field()
   public media_locator: string;
+
+  @Field()
+  public media_type: string;
 }
--- skillsly_ag/src/application/service/post/request-input/create_post.request_input.ts
+++ skillsly_ag/src/application/service/post/request-input/create_post.request_input.ts
@@ -1,13 +1,15 @@
 import {Id, Nullable} from '@application/common/type/common_types';
+import {Upload} from 'graphql-upload';
 
 export default interface CreatePostRequestInput {
   owner_id: Id;
-  description: string;
+  description: Nullable<string>;
   privacy: string;
   content_element: Array<PostContentElement>;
 }
 interface PostContentElement {
-  content_element_id: Id;
   description: Nullable<string>;
-  media_locator: Nullable<string>;
+  // media: Upload;
+  media_locator: string;
+  media_type: string;
 }
