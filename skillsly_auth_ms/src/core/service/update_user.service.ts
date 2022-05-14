a2458326e44eb8c6daca10fb6cddf468b298120c
Jonathan Lopez Castellanos <jolopezca@unal.edu.co>
2022-05-14 11:03:10 -0500

    Some fixes to auth services logic

--- skillsly_auth_ms/src/core/service/update_user.service.ts
+++ skillsly_auth_ms/src/core/service/update_user.service.ts
@@ -6,7 +6,6 @@ import { Inject, Logger } from '@nestjs/common'
 import UpdateUserInteractor from '@core/domain/use-case/interactor/update_user.interactor'
 import { UserDITokens } from '@core/domain/di/user_di_tokens'
 import { UserNotFoundException } from '@core/domain/use-case/exception/auth.exception'
-import { User } from '@core/domain/entity/user'
 
 export class UpdateUserService implements UpdateUserInteractor {
   private readonly logger: Logger = new Logger(UpdateUserService.name);
@@ -21,10 +20,6 @@ export class UpdateUserService implements UpdateUserInteractor {
     const existing_user: UserDTO = await this.gateway.findOne({ id: user_id });
     if (!existing_user)
       throw new UserNotFoundException();
-    const user_to_update = new User({
-      ...existing_user,
-
-    });
     const updated_user: UserDTO = await this.gateway.partialUpdate({ id: user_id },  {
       is_two_factor_auth_enabled,
       two_factor_auth_secret,
