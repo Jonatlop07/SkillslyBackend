3858076fdf102b69d998d63a977493dec44c14c2
Jonathan Lopez Castellanos <jolopezca@unal.edu.co>
2022-06-03 23:17:59 -0500

    Cloud done

---
+++ infra/k8s/cloud/delete.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+kubectl delete service skillsly-ldap-admin-srv ;
+kubectl delete deployment skillsly-ldap-admin-depl ;
+kubectl delete service skillsly-ldap-srv ;
+kubectl delete deployment skillsly-ldap-depl ;
+kubectl delete persistentvolumeclaims skillsly-ldap-pv-claim ;
+kubectl delete persistentvolume skillsly-ldap-persistent-volume ;
+kubectl delete configmap skillsly-ldap-env-config ;
+
+kubectl delete service skillsly-user-ms-srv ;
+kubectl delete deployment skillsly-user-ms-depl ;
+kubectl delete configmap skillsly-user-ms-env-config ;
+
+kubectl delete service skillsly-auth-ms-srv ;
+kubectl delete deployment skillsly-auth-ms-depl ;
+kubectl delete configmap skillsly-auth-ms-env-config ;
+
+kubectl delete service skillsly-ag-srv ;
+kubectl delete deployment skillsly-ag-depl ;
+kubectl delete configmap skillsly-ag-env-config ;
+kubectl delete service skillsly-ag-redis-srv ;
+kubectl delete deployment skillsly-ag-redis-depl ;
+kubectl delete configmap skillsly-ag-redis-config ;
---
+++ infra/k8s/cloud/init_2.sh
@@ -0,0 +1,3 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.0/deploy/static/provider/cloud/deploy.yaml ;
+kubectl apply -f ./ingress/skillsly-ingress-class.yaml
+kubectl apply -f ./ingress/skillsly-ingress.yaml ;
