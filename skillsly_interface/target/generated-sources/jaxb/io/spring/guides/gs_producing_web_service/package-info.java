30ced2bcd5e52c4dcd1f18a88d9a16303f987345
Victor Barragan <vbarraganp@unal.edu.co>
2022-06-07 20:36:02 -0500

    generated consume and expose .jar

--- skillsly_interface/target/generated-sources/jaxb/io/spring/guides/gs_producing_web_service/package-info.java
+++ skillsly_interface/target/generated-sources/jaxb/io/spring/guides/gs_producing_web_service/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.06.06 a las 07:43:07 PM COT 
+// Generado el: 2022.06.07 a las 08:34:21 PM COT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
