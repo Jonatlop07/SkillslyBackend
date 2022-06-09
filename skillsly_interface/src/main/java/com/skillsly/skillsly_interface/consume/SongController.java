66992415c1611f4cad5cd604f9586f599b35075c
Wilson Arevalo <wnarevalor@unal.edu.co>
2022-06-08 22:35:28 -0500

    allowed cross origin requests

--- skillsly_interface/src/main/java/com/skillsly/skillsly_interface/consume/SongController.java
+++ skillsly_interface/src/main/java/com/skillsly/skillsly_interface/consume/SongController.java
@@ -1,6 +1,7 @@
 package com.skillsly.skillsly_interface.consume;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class SongController {
   @Autowired
   private SongClient songClient;
 
+  @CrossOrigin(origins = "*")
   @GetMapping("/song/{id}")
   public Response getUser(@PathVariable("id") String id) {
 
