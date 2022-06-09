d2bd937d9d476393f2ba946905e20c500938bd1a
Wilson Arevalo <wnarevalor@unal.edu.co>
2022-06-08 22:06:35 -0500

    removed requestmapping because ...

--- skillsly_interface/src/main/java/com/skillsly/skillsly_interface/consume/SongController.java
+++ skillsly_interface/src/main/java/com/skillsly/skillsly_interface/consume/SongController.java
@@ -3,19 +3,17 @@ package com.skillsly.skillsly_interface.consume;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.consumingwebservice.wsdl.Response;
 
 @RestController
-@RequestMapping("/")
 public class SongController {
 
   @Autowired
   private SongClient songClient;
 
-  @GetMapping("song/{id}")
+  @GetMapping("/song/{id}")
   public Response getUser(@PathVariable("id") String id) {
 
     Response res = songClient.getSong(id);
