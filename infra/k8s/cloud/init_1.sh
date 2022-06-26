56108b9b52e73df59252b3af6e90d80b51adcfcd
Jonathan Lopez Castellanos <jolopezca@unal.edu.co>
2022-06-25 19:33:32 -0500

    Cambios para el despliegue en cloud

--- infra/k8s/cloud/init_1.sh
+++ infra/k8s/cloud/init_1.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
 
+# Notification MQ deployment
+
+kubectl apply -f ./notification_mq/skillsly-notification-mq-rbac.yaml
+kubectl apply -f ./notification_mq/skillsly-notification-mq-secret.yaml
+kubectl apply -f ./notification_mq/skillsly-notification-mq-config-map.yaml
+kubectl apply -f ./notification_mq/skillsly-notification-mq-statefulset.yaml
+
+# Notification Handler MS deployment
+
+kubectl apply -f ./notification_handler/skillsly-notification-handler-redis-config-map.yaml
+kubectl apply -f ./notification_handler/skillsly-notification-handler-redis-depl.yaml
+kubectl apply -f ./notification_handler/skillsly-notification-handler-ms-env-config-map.yaml
+kubectl apply -f ./notification_handler/skillsly-notification-handler-backend-config.yaml
+kubectl apply -f ./notification_handler/skillsly-notification-handler-ms-depl.yaml
+
+# Notification MS deployment
+
+kubectl apply -f ./notification/skillsly-notification-ms-env-config-map.yaml ;
+kubectl apply -f ./notification/skillsly-notification-ms-depl.yaml ;
+
 # Ldap deployment
 
 kubectl apply -f ./ldap/skillsly-ldap-env-config-map.yaml ;
 kubectl apply -f ./ldap/skillsly-ldap-persistent-volume.yaml ;
 kubectl apply -f ./ldap/skillsly-ldap-depl.yaml ;
 kubectl apply -f ./ldap/skillsly-ldap-admin-depl.yaml ;
+
+# Service MS deployment
+
+kubectl apply -f ./service/skillsly-service-ms-depl.yaml
+
+# Chat MS deployment
+
+kubectl apply -f ./chat/skillsly-chat-ms-depl.yaml
+
+# Comment MS deployment
+
+kubectl apply -f ./comment/skillsly-comment-ms-env-config-map.yaml
+kubectl apply -f ./comment/skillsly-comment-ms-depl.yaml
+
+# Posts MS deployment
+
+kubectl apply -f ./post/skillsly-posts-ms-depl.yaml
+
+# Story MS deployment
+
+kubectl apply -f ./story/skillsly-story-ms-env-config-map.yaml ;
+kubectl apply -f ./story/skillsly-story-ms-depl.yaml ;
 
 # User MS deployment
 
@@ -27,3 +69,9 @@ kubectl apply -f ./ag/skillsly-ag-depl.yaml ;
 # Interface deployment
 
 kubectl apply -f ./interface/skillsly-interface-depl.yaml
+
+#db config
+#gcloud sql connect skillsly-name-db --user=postgres
+#\c postgres
+#Paste script
+#SELECT schema_name FROM information_schema.schemata;
