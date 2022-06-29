3086df605b6975b54fabf4a2765f56a665843db8
Angel M. Gonzalez <angegonzalez@unal.edu.co>
2022-06-28 20:52:05 -0500

    Correcting public cloud services

--- infra/k8s/cloud/public/delete.sh
+++ infra/k8s/cloud/public/delete.sh
@@ -2,6 +2,5 @@
 
 kubectl delete -f ./ag/skillsly-ag-service.yaml
 kubectl delete -f ./interface/skillsly-interface-service.yaml
-kubectl delete -f ./ldap/skillsly-ldap-service.yaml
 kubectl delete -f ./notification/skillsly-notification-service.yaml
 kubectl delete -f ./wa/skillsly-wa-service.yaml
--- infra/k8s/cloud/public/init_1.sh
+++ infra/k8s/cloud/public/init_1.sh
@@ -2,6 +2,5 @@
 
 kubectl apply -f ./ag/skillsly-ag-service.yaml
 kubectl apply -f ./interface/skillsly-interface-service.yaml
-kubectl apply -f ./ldap/skillsly-ldap-service.yaml
 kubectl apply -f ./notification/skillsly-notification-service.yaml
 kubectl apply -f ./wa/skillsly-wa-service.yaml
